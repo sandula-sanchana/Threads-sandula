b2406015b5374ca2d7f038c8fb08aec5898ee287
sandula-sanchana <sandulasanchana@gmail.com>
2025-10-01 10:33:23 +0530

    module 3

--- extend_thread_class/src/main/java/edu/ijse/Main.java
+++ extend_thread_class/src/main/java/edu/ijse/Main.java
@@ -17,6 +17,11 @@ class A extends Thread {
     public void run() {
         for (int i = 1; i < 10; i++) {
             System.out.println("A");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
     }
@@ -26,6 +31,11 @@ class B extends Thread {
     public void run() {
        for (int i = 1; i <10; i++) {
            System.out.println("B");
+           try {
+               Thread.sleep(1000);
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
        }
     }
 }
