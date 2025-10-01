bb9a1a1ebfab17f5cee5ff36fbb1499b1bdca40f
sandula-sanchana <sandulasanchana@gmail.com>
2025-10-01 11:57:50 +0530

    updates

---
+++ syncronized/src/main/java/edu/ijse/Main.java
@@ -0,0 +1,39 @@
+package edu.ijse;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+       Adder adder=new Adder();
+        Thread t1 = new Thread(()->{
+            for(int i=0;i<1000;i++){
+                adder.Increment();
+            }
+        });
+        Thread t2 = new Thread(()->{
+             for(int i=0;i<1000;i++){
+                 adder.Increment();
+             }
+        });
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(adder.num);
+    }
+
+    static class Adder{
+        int num=0;
+
+        public synchronized void  Increment(){
+            num++;
+        }
+//        public synchronized void  Decrement(){
+//            num--;
+//        }
+    }
+}
---
+++ thread_piority/src/main/java/edu/ijse/Main.java
@@ -0,0 +1,26 @@
+package edu.ijse;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1=new Thread(()->{
+            for(int i=0;i<10;i++){
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        Thread t2=new Thread(()->{
+            for(int i=0;i<10;i++){
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        t1.start();
+        t1.join();//pause the current thread untill the t1 is complete(pause main thread here)
+        System.out.println(t1.isAlive());
+        t2.start();
+        t2.join();
+        System.out.println(t2.isAlive());//check if the calling thread is running or not
+        System.out.println("bb");//this prints 1st because the main thread starts first and the proceed to the other threads
+
+    }
+}
