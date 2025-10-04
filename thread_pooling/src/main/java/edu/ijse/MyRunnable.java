12d202a9e83c02f494d9117e6060c27f930dd544
sandula-sanchana <sandulasanchana@gmail.com>
2025-10-04 09:49:35 +0530

    add producer-consumer implementation with thread pooling

--- thread_pooling/src/main/java/edu/ijse/MyRunnable.java
+++ thread_pooling/src/main/java/edu/ijse/MyRunnable.java
@@ -1,26 +1,18 @@
 package edu.ijse;
 
-public class MyRunnable implements Runnable{
-    private String threadName;
-    public MyRunnable(String threadName) {
-        this.threadName = threadName;
+class MyRunnable implements Runnable {
+    String  name;
+    public MyRunnable(String name) {
+        this.name = name;
     }
-
     @Override
-    public void run() {
-        System.out.println(threadName + " is running");
+        public void run() {
+        System.out.println(name);
         try {
-            for (int i = 0; i < 5; i++) {
-                System.out.println(threadName + " - Count: " + i);
-                Thread.sleep(500);
-            }
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
-            System.out.println(threadName + " interrupted.");
+            throw new RuntimeException(e);
         }
-        System.out.println(threadName + " has finished execution.");
-    }
-
-    public static void main(String[] args) {
-
+        System.out.println("run end");
     }
 }
